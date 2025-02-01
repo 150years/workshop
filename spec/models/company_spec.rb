7ca910a00c05ad0f2fe104a818471369cad85dcf
the1723 <hello@paveldavydov.com>
2025-02-01 19:31:03 +0100

    feat(company): added

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end

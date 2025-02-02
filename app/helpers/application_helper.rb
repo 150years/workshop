e44add0853aaf6afd70965c480f779b04361b623
the1723 <hello@paveldavydov.com>
2025-02-02 10:49:17 +0100

    feat(mobile menu): improved styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def mobile_menu_button_class(path)
+    "btn #{request.path.include?(path) ? 'btn--secondary' : 'btn--borderless'}"
+  end
 end

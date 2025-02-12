394be5690f66756ba008c38eeae88dffdafeb36c
the1723 <hello@paveldavydov.com>
2025-02-12 22:35:57 +0100

    chore: lint

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -1,11 +1,12 @@
-Rails.application.config.action_view.field_error_proc = proc do |html_tag, instance|
+# frozen_string_literal: true
+Rails.application.config.action_view.field_error_proc = proc do |html_tag, _instance|
   # Use Nokogiri to parse and modify the HTML tag
   doc = Nokogiri::HTML::DocumentFragment.parse(html_tag)
   element = doc.children.first
 
   if element && %w[input select textarea].include?(element.name)
     element['class'] = [element['class'], 'is-invalid'].compact.join(' ')
-    element.to_s.html_safe
+    element.to_s.html_safe # rubocop:disable Rails/OutputSafety
   else
     html_tag
   end

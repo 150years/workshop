ada882646698ee56812002f2e18a3399a9834a50
the1723 <hello@paveldavydov.com>
2025-02-12 21:00:15 +0100

    chore(bullet): disable alerts and console logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,9 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = false
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
+    Bullet.bullet_logger = false
+    Bullet.console       = false
+    Bullet.rails_logger  = false
     Bullet.add_footer    = true
   end
 

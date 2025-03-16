71a7e4109b3dc463c7bba27643465084ebba4924
the1723 <hello@paveldavydov.com>
2025-03-16 17:49:19 +0100

    fix(UnitConverter): set rounding to 4 (0.1 mm)

--- app/helpers/unit_converter.rb
+++ app/helpers/unit_converter.rb
@@ -4,18 +4,18 @@ module UnitConverter
   def self.mm_to_m(value)
     return nil if value.nil?
 
-    (value.to_f / 1000).round(5)
+    (value.to_f / 1000).round(4)
   end
 
   def self.mm2_to_m2(value)
     return nil if value.nil?
 
-    (value.to_f / 1_000_000).round(5)
+    (value.to_f / 1_000_000).round(4)
   end
 
   def self.mm2_to_ft2(value)
     return nil if value.nil?
 
-    (mm2_to_m2(value) * 10.7639).round(5)
+    (mm2_to_m2(value) * 10.7639).round(4)
   end
 end

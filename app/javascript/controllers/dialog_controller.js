5596fd6c49b0dbfe607534b525d57197522773b1
the1723 <hello@paveldavydov.com>
2025-04-03 18:21:34 +0200

    fix(dialog controller) not working properly in table

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -3,19 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "menu" ]
 
-  show() {
-    this.menuTarget.show()
+  connect() {
+    this.shown = false
   }
 
   showModal() {
+    if (this.shown) return
+
     this.menuTarget.showModal()
+    this.shown = true
   }
 
-  close() {
+  close(event) {
+    event?.stopPropagation();
     this.menuTarget.close()
+    this.shown = false
   }
 
-  closeOnClickOutside({ target }) {
-    target.nodeName === "DIALOG" && this.close()
+  closeOnClickOutside(event) {
+    if (event.target.nodeName === "DIALOG") {
+      event.stopPropagation();
+      this.close();
+    }
   }
 }

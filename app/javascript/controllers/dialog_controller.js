5dd46d602a13139c428a4fe4b7eb980a4a3d6b3c
the1723 <hello@paveldavydov.com>
2025-04-03 19:38:06 +0200

    feat(Order): added actions menu for products

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -1,15 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "menu" ]
+  static targets = ["menu", "frame"]
 
   connect() {
     this.shown = false
   }
 
-  showModal() {
+  showModal(event) {
     if (this.shown) return
+    if (event.target.closest('#dropdown')) return
 
+    this.menuTarget.showModal()
+    this.shown = true
+  }
+
+  show(event) {
+    event.stopPropagation();
     this.menuTarget.showModal()
     this.shown = true
   }
@@ -25,5 +32,16 @@ export default class extends Controller {
       event.stopPropagation();
       this.close();
     }
+  }
+
+  openFromLink(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const url = event.currentTarget.getAttribute("href")
+
+    this.frameTarget.src = url
+    this.menuTarget.showModal()
+    this.shown = true
   }
 }

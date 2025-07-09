4b3047e22c16802c701fbf6685c64ef00a66f108
Sergey Lebedev <bytheair@gmail.com>
2025-07-09 18:11:41 +0300

    Photo before and after

--- app/models/defect_list_item.rb
+++ app/models/defect_list_item.rb
@@ -26,16 +26,23 @@
 class DefectListItem < ApplicationRecord
   belongs_to :defect_list
   belongs_to :product
-  has_many_attached :photos
+  # has_many_attached :photos
+  has_one_attached :photo_before
+  has_one_attached :photo_after
 
   enum :status, {
     other: 'Other / อื่น',
     scratches: 'Scratches / รอย',
+    cover: 'Cover / อลูมิเนียม​cover',
+    drain_holes: 'Drain holes / รูบายน้ำำ',
     size_issue: 'Size / ขนาด',
+    screw: 'Screw / สกรู',
+    sealant_rubber: 'Rubber / ยาง',
     sealant_color: 'Sealant color / สีซีลแลนท์',
-    sealant_quality: 'Sealant quality / คุณภาพของซีลแลนท์',
+    sealant_quality: 'Sealant quality / คุณภาพซีลแลนท์',
     sealant_size: 'Sealant size / ขนาดซีลแลนท์',
     gaps: 'Gaps / ช่อง',
-    lock_and_handle: 'Lock and handle / ล็อคและจับ'
+    lock_and_handle: 'Lock & handle / ล็อคและจับ',
+    settings: 'Settings / ตั้ง'
   }
 end

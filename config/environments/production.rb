d1dae9216d3f83174b1d304ae0c5d3fda964c2ec
Sergey Lebedev <bytheair@gmail.com>
2025-11-03 20:46:44 +0300

    r2-4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :r2
-
+  config.active_storage.compute_checksums = false
+  
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
 
